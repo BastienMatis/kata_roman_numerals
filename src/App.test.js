14e917962ef20afa7d9a6ef0e12457cbb9a67fcc
Bastien Matis <matis.bastien@outlook.fr>
2023-11-08 09:50:21 +0100

    test 10 = X

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import RomanNumerals from './components/RomanNumerals';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+describe('RomanNumerals Component', () => {
+  it('converts user input to Roman numeral and displays the result', () => {
+    // Render the component on a virtual DOM for testing
+    const { getByLabelText, getByRole, getByText } = render(<RomanNumerals />);
+
+    // Find the input field and button for interactions
+    const input = screen.getByLabelText(/arabic number/i);
+    const convertButton = screen.getByRole('button', { name: /convert/i });
+
+    // Simulate user typing "10" into the input field
+    fireEvent.change(input, { target: { value: '10' } });
+
+    // Simulate user clicking the "Convert" button
+    fireEvent.click(convertButton);
+
+    // Assert that the expected output is rendered in the DOM
+    expect(screen.getByText('X')).toBeInTheDocument();
+  });;
 });
