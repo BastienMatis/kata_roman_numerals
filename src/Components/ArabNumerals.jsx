dc245ae353bd20e2e56c641df05671358162c2c7
GaburaisuVGC <matthieu.barbe01@gmail.com>
2023-11-08 14:09:12 +0100

    added first check

--- src/Components/ArabNumerals.jsx
+++ src/Components/ArabNumerals.jsx
@@ -9,65 +9,44 @@ class ArabNumerals extends Component {
             value: '',
             roman: '',
             errorMessage: '',
-            number: ''
+            number: '',
+            romanNumerals: ["I", "V", "X", "L", "C", "D", "M"]
         }
     }
 
     handleChange = (event) => {
         const inputValue = event.target.value;
-        if (inputValue >= 1 && inputValue <= 3999) {
-            this.setState({ value: inputValue, errorMessage: '' });
-        } else {
-            this.setState({ errorMessage: 'Veuillez entrer un nombre entre 1 et 3999' });
-        }
+        this.setState({ value: inputValue, errorMessage: '' });
     }
 
     handleSubmit = (event) => {
         event.preventDefault();
-        this.setState({ roman: this.convertToRoman(this.state.value), number: this.state.value });
-    }
-
-    convertToRoman = (num) => {
-        let roman = '';
-        const romanNumerals = {
-            M: 1000,
-            CM: 900,
-            D: 500,
-            CD: 400,
-            C: 100,
-            XC: 90,
-            L: 50,
-            XL: 40,
-            X: 10,
-            IX: 9,
-            V: 5,
-            IV: 4,
-            I: 1
-        };
-
-        for (let i in romanNumerals) {
-            while (num >= romanNumerals[i]) {
-                roman += i;
-                num -= romanNumerals[i];
-            }
+        const romanNumber = this.state.value;
+        const romanNumerals = this.state.romanNumerals;
+        // if romanNumber contains a number, special character, space, or letter not in the romanNumerals array, return error message
+        if (romanNumber.match(/[0-9]/g) || romanNumber.match(/[^IVXLCDM]/g) || romanNumber.match(/\s/g)) {
+            this.setState({ errorMessage: 'Veuillez entrer un chiffre romain valide' });
+        } else {
+            // this.setState({ number: this.convertToArab(romanNumber) });
         }
-        return roman;
+
     }
+
 
     render() {
         return (
 <div className="romanNumerals">
     <Form onSubmit={this.handleSubmit}>
         <Form.Group controlId="formBasicEmail">
-            <Form.Label className="form-label">Convertir un chiffre arabe en chiffre romain</Form.Label>
-            <Form.Control type="number" className="form-control" placeholder="Entrez un nombre entier" onChange={this.handleChange} />
+            <Form.Label className="form-label">Convertir un chiffre romain en chiffre arabe</Form.Label>
+            <Form.Control type="text" className="form-control" placeholder="Entrez un chiffre romain" onChange={this.handleChange} />
             <p className="error-message">{this.state.errorMessage}</p>
         </Form.Group>
         <Button variant="primary" type="submit" className="submit-button">
             Convertir
         </Button>
     </Form>
-    <h1 className="roman-result">{this.state.roman === '' ? '' : `${this.state.number}: ${this.state.roman}`}</h1>
+    <h1 className="roman-result">{this.state.number === '' ? '' : `${this.state.roman}: ${this.state.number}`}</h1>
 </div>
         );
     }
